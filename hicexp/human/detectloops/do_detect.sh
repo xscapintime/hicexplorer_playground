57e9f4ee5cca9bb5112dfc3f17a87d8b06d18cae
xscapintime <louiesxscape@gmail.com>
2021-04-14 16:51:09 +0800

    .cool matrix meant to be the file before cooler dump

--- hicexp/human/detectloops/do_detect.sh
+++ hicexp/human/detectloops/do_detect.sh
@@ -1,7 +1,7 @@
-for f in ../cool/*.gz
+for f in ../cool/*.cooler
 do
 	bn=`basename $f`
-	rn=`echo ${bn} | sed "s/.matrix.gz//g"`
+	rn=`echo ${bn} | sed "s/.iced.matrix.gz//g"`
 
 	qsub -N ${rn}_hicDetectLoops -v input=${f},output=${rn}.loops.bedgraph detectloops.pbs
 done
