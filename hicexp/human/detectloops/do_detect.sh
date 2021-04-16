92471cdea4f7135cbdee57c4057b08697f4ffa2f
liyang <liyang@nfkd.cn>
2021-04-16 21:59:01 +0800

    cooler cload without balance works! (loop calling)

--- hicexp/human/detectloops/do_detect.sh
+++ hicexp/human/detectloops/do_detect.sh
@@ -1,7 +1,12 @@
-for f in ../cool/*.cool
+for f in ../cooler_nobln/*.cool
 do
 	bn=`basename $f`
-	rn=`echo ${bn} | sed "s/.iced.matrix.gz//g"`
-
-	qsub -N ${rn}_hicDetectLoops -v input=${f},output=${rn}.loops.bedgraph detectloops.pbs
+	#rn=`echo ${bn} | sed "s/.iced.matrix.gz//g"`
+	
+	if ! [ -f ${bn}.loops.bedgraph ]
+	then
+		echo ${bn}.loops.bedgraph
+		qsub -N ${bn}_hicDetectLoops -v input=${f},output=${bn}.loops.bedgraph detectloops.pbs
+		sleep 0.1
+	fi
 done
